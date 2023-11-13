8cd865e5c0778cce39726a5eac3eb8448e34ae4c
Joshua Jacobs <joshua.jacobs126@gmail.com>
2023-11-13 17:53:49 -0600

    Add project files.

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RedisExploration.Controllers
+{
+    [ApiController]
+    Route["controller"]
+    public class TaskController : Controller
+    {
+       
+    }
+}
---
+++ Task.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RedisExploration
+{
+    public class Task : Controller
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateOnly DueDate { get; set; }
+    }
+}
